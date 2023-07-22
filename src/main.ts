386e76c7845503fda4ebf6427b69c9f1c94ff86c
AAVision <75311052+AAVision@users.noreply.github.com>
2023-07-22 12:47:28 +0300

    Upgrading to angular 16 and adding pwa

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { enableProdMode, importProvidersFrom, isDevMode } from '@angular/core';
 import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { HttpClientModule } from '@angular/common/http'
 import { environment } from './environments/environment.prod';
 import { APP_ROUTES } from './app/app-routing.module';
 import { RouterModule } from '@angular/router';
+import { provideServiceWorker } from '@angular/service-worker';
 
 
 if (environment.production) {
@@ -13,9 +14,10 @@ if (environment.production) {
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(
-      HttpClientModule,
-      BrowserModule,
-      RouterModule.forRoot(APP_ROUTES)
-    )]
+    importProvidersFrom(HttpClientModule, BrowserModule, RouterModule.forRoot(APP_ROUTES)),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+]
 }).catch(err => console.error(err));
