894bb94c0ec11a8e912f17f48b4ad5216396069c
Ali <75311052+AAVision@users.noreply.github.com>
2023-07-31 14:34:07 +0300

    Code enhancements

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,10 +1,11 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from 'src/app/shared/navbar/navbar.component';
 import { FooterComponent } from 'src/app/shared/footer/footer.component';
 import { Quote } from 'src/app/models/quote';
 import { QuoteService } from 'src/app/services/quote.service';
-import { Subscription } from 'rxjs';
+import { catchError, finalize, map, of } from 'rxjs';
 import * as htmlToImage from 'html-to-image';
 import { saveAs } from 'file-saver';
 
@@ -29,41 +30,45 @@ export class HomeComponent implements OnInit {
   isLoaded = false;
 
   allQuotes: Quote[] = [];
-  quotesSub: Subscription;
   randomNumber: number;
   quote: Quote;
 
   constructor(private _quoteService: QuoteService) { }
 
   ngOnInit(): void {
-    this.quotesSub = this._quoteService.getQuote()
-      .subscribe({
-        next: (data: Quote[]) => {
+    this._quoteService.getQuote()
+      .pipe(
+        map((data: Quote[]) => {
           if (data) {
             this.emoji = this.emojis[Math.floor(Math.random() * this.emojis.length)];
             this.randomNumber = this.getRandomInt(data.length);
             this.quote = data[this.randomNumber];
-            this.isLoaded = true;
           }
-        },
-        error: err => console.error('An error occurred', err)
-      });
-
+        }),
+        takeUntilDestroyed(),
+        catchError((err) => {
+          console.log(err);
+          return of([]);
+        }),
+        finalize(() => {
+          this.isLoaded = true;
+        })
+      ).subscribe();
   }
 
-  getRandomInt(max): number {
+  getRandomInt(max: number): number {
     return Math.floor(Math.random() * max);
   }
 
-  downloadImage() {
-    var dt = new Date( "December 25, 1995 23:15:20" );
+  downloadImage(): void {
+    var dt = new Date("December 25, 1995 23:15:20");
     htmlToImage.toBlob(this.allDiv.nativeElement)
       .then(function (blob) {
         saveAs(blob, `icareforyou_${dt.getTime()}.png`);
       });
   }
 
-  speakText() {
+  speakText(): void {
     let utterance = new SpeechSynthesisUtterance(this.quote?.text + 'By ' + this.quote?.from);
     speechSynthesis.speak(utterance);
   }
