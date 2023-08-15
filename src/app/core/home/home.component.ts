7ea213fe771d088f2b7f76f02f2d979da714ffc2
Ali <75311052+AAVision@users.noreply.github.com>
2023-08-15 12:38:05 +0300

    Upgrading and SEO fixes

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,11 +1,11 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from 'src/app/shared/navbar/navbar.component';
 import { FooterComponent } from 'src/app/shared/footer/footer.component';
 import { Quote } from 'src/app/models/quote';
 import { QuoteService } from 'src/app/services/quote.service';
-import { catchError, finalize, map, of } from 'rxjs';
+import { Subject, catchError, finalize, map, of, takeUntil } from 'rxjs';
 import * as htmlToImage from 'html-to-image';
 import { saveAs } from 'file-saver';
 
@@ -20,9 +20,10 @@ import { saveAs } from 'file-saver';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   @ViewChild('allDiv') allDiv: ElementRef;
+  private onDestroy$: Subject<void> = new Subject();
 
   emojis = ["😃", "😍", "😎", "😏", "🙂", "🙋‍♀️", "🆗", "💎", "🌝", "😘", "✌", "🙌", "🔥", "☀", "🎈", "🌹", "🎀", "🏁", "🍀", "👀", "💝", "💙", "👌", "❤", "💪", "🍸", "🔑", "💖", "🌟", "🎉", "🌺", "🎶", "🏆", "🍓", "💘", "💜", "👊", "💋", "😘", "🙏", "👋", "💃", "💎", "🌙", "🎁", "⛄", "🌊", "⛵", "💰", "💛", "💚"]
   emoji = "";
@@ -45,7 +46,7 @@ export class HomeComponent implements OnInit {
             this.quote = data[this.randomNumber];
           }
         }),
-        takeUntilDestroyed(),
+        takeUntil(this.onDestroy$),
         catchError((err) => {
           console.log(err);
           return of([]);
@@ -54,6 +55,11 @@ export class HomeComponent implements OnInit {
           this.isLoaded = true;
         })
       ).subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.onDestroy$.next();
+    this.onDestroy$.complete();
   }
 
   getRandomInt(max: number): number {
